eb559590cf615a6fa8c146b8af19e64361bcf62d
MMPjayarathne <pramudithajayarathna@gmail.com>
2024-08-09 00:14:05 +0530

    Add the contact me section

---
+++ frontend/src/app/components/contact-me/contact-me.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@Component({
+  selector: 'app-contact-me',
+  standalone: true,
+  imports: [FlexLayoutModule],
+  templateUrl: './contact-me.component.html',
+  styleUrl: './contact-me.component.scss'
+})
+export class ContactMeComponent {
+
+}
