ba1b1aa70a571e2240870ebb208685ab4b06e2ad
MMPjayarathne <pramudithajayarathna@gmail.com>
2024-08-09 00:13:21 +0530

    Update the main colors as variables

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { MainComponent } from '../../components/main/main.component';
 import { AboutMeComponent } from '../../components/about-me/about-me.component';
 import { MySkillsComponent } from '../../components/my-skills/my-skills.component';
 import { MyProjectsComponent } from '../../components/my-projects/my-projects.component';
+import { ContactMeComponent } from '../../components/contact-me/contact-me.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent,MainComponent,AboutMeComponent,MySkillsComponent,MyProjectsComponent],
+  imports: [NavBarComponent,MainComponent,AboutMeComponent,MySkillsComponent,MyProjectsComponent,ContactMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
