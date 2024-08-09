1a511701d2c9574bb11e4c58c8046837b332df0d
MMPjayarathne <pramudithajayarathna@gmail.com>
2024-08-10 00:18:35 +0530

    Add a blogs section and the footer

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -5,11 +5,13 @@ import { AboutMeComponent } from '../../components/about-me/about-me.component';
 import { MySkillsComponent } from '../../components/my-skills/my-skills.component';
 import { MyProjectsComponent } from '../../components/my-projects/my-projects.component';
 import { ContactMeComponent } from '../../components/contact-me/contact-me.component';
+import { BlogsComponent } from '../../components/blogs/blogs.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent,MainComponent,AboutMeComponent,MySkillsComponent,MyProjectsComponent,ContactMeComponent],
+  imports: [NavBarComponent,MainComponent,AboutMeComponent,MySkillsComponent,MyProjectsComponent,ContactMeComponent,BlogsComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
