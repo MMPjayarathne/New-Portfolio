2c13f651a703b4bb7616b95eeb37b8736bf850d8
MMPjayarathne <pramudithajayarathna@gmail.com>
2024-08-25 23:32:07 +0530

    Add database models and context

---
+++ backend/backend/Models/PersonalInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    public class PersonalInfo
+    {
+        [Key]
+        public int Id { get; set; } // Primary key
+
+        [Column(TypeName = "text")]
+        public string Identifier { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Content { get; set; }
+    }
+}
---
+++ backend/backend/Models/SqlDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+    public partial class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+            : base(options) { }
+
+        // DbSet for PersonalInfo entity
+        public virtual DbSet<PersonalInfo> PersonalInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonalInfo>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
+
